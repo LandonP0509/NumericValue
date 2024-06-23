d0c478792f85e3ba0a35c26bd349b9ebd923a473
LandonP0509 <146989039+LandonP0509@users.noreply.github.com>
2024-06-23 17:23:27 -0500

    Add files via upload

---
+++ NumericValue.cs
@@ -0,0 +1,17 @@
+using System
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Please enter the length of your rectangle: ");
+        double length = Convert.ToDouble(Console.ReadLine());
+
+        console.Write("Please enter the width of your rectangle: ")
+        double width = Convert.ToDouble(console.ReadLine())
+
+        double rArea = length * width;
+
+        Console.WriteLine("The area of your rectangle is " + rArea)
+    }
+}
